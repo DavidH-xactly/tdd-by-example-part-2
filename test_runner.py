9d85f5ec1afed8ea2a3d36f4737f19b36c7d7c56
David Hammond <dhammond@xactlycorp>
2020-06-18 14:28:15 -0600

    tear down and basic test result in place

--- test_runner.py
+++ test_runner.py
@@ -1,3 +1,8 @@
+class TestResult:
+    def summary(self):
+        return "1 run, 0 failed"
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -5,33 +10,50 @@ class TestCase:
     def set_up(self):
         pass
 
+    def tear_down(self):
+        pass
+
     def run(self):
+        result = TestResult()
         self.set_up()
         method = getattr(self, self.name)
         method()
+        self.tear_down()
+        return result
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
+        self.log = ''
 
     def set_up(self):
-        self.was_run = None
-        self.log = "set_up "
+        self.log += "set_up "
 
     def test_method(self):
-        self.set_up()
-        self.was_run = 1
         self.log += 'test_method '
+
+    def tear_down(self):
+        self.log += 'tear_down'
 
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.test = WasRun('test_method')
+
     def test_template_method(self):
-        test = WasRun('test_method')
+        self.test.run()
 
-        test.run()
-        assert(test.log == 'set_up test_method ')
+        assert(self.test.log == 'set_up test_method tear_down')
+
+    def test_result(self):
+        result = self.test.run()
+
+        assert(result.summary() == "1 run, 0 failed")
+
+    def tear_down(self):
+        pass
 
 
-TestCaseTest('test_running')
-TestCaseTest('test_set_up')
+TestCaseTest('test_template_method').run()
+TestCaseTest('test_result').run()
